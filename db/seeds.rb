1ce7076f8ca33cae2eab498fc6eee7147e50ce22
Agathe Laburthe TOlra <agathe.laburthetolra@gmail.com>
2020-11-12 07:59:23 -0600

    simple airbnb back working - need to add some style

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating some flats...'
+Flat.destroy_all
+
+4.times do
+  Flat.create(name: Faker::Artist.name,
+               address: Faker::Games::SuperMario.location,
+               description: Faker::Coffee.notes,
+               price_per_night: Faker::Number.decimal(l_digits: 3, r_digits: 2),
+               number_of_guests: Faker::Number.decimal_part(digits: 2))
+end
+
+puts 'Done! you have 4 new flats'
